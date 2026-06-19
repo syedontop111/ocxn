0f02fcdecf14a5fa5f86bbb1424bdee7b54f1bc3
syedontop111 <syedontop111@gmail.com>
2026-06-20 01:49:00 +0530

    Update Button.jsx

--- Button.jsx
+++ Button.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-const cn = (...classes) => classes.filter(Boolean).join(" ");
 
-const Separator = ({ className, orientation = "horizontal", ...props }) => (
-  <div
-    className={cn(
-      "shrink-0 bg-border",
-      orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
-      className
-    )}
-    {...props}
-  />
-);
-
-export { Separator };
+export function Button({
+  children,
+  className = "",
+  ...props
+}) {
+  return (
+    <button
+      className={`px-4 py-2 rounded-lg bg-violet-600 text-white hover:bg-violet-700 ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
